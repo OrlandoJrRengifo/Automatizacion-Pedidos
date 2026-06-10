52851d33a7df613e1f7044d832d70d350ae78761
Orlando Jr. Rengifo <107599003+OrlandoJrRengifo@users.noreply.github.com>
2026-06-09 19:26:58 -0500

    correcion puerto

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Servidor corriendo en http://localhost:${process.env.PORT ?? 3000}`);
   console.log(`Swagger en http://localhost:${process.env.PORT ?? 3000}/docs`);
 }

5c3d6386d0d6565ce2e64c8500a1524c32a8fd41
Orlando Jr. Rengifo <107599003+OrlandoJrRengifo@users.noreply.github.com>
2026-06-09 20:57:17 -0500

    Config para el deploy del script de python

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: frontendUrl 
       ? [frontendUrl, 'http://localhost:5173'] 
-      : '*', // Si por alguna razón no existe la variable, permite cualquiera temporalmente
+      : '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
